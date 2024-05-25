26d6882b75e3a2ffe4983051d3ae4da0bda25b8f
Andrei Neacsu <58575812+prycaustic@users.noreply.github.com>
2024-05-25 03:25:31 -0500

    Make album viewer styles a bit more minimalist

--- src/lib/comp/SongSelector.svelte
+++ src/lib/comp/SongSelector.svelte
@@ -56,30 +56,32 @@
 </script>
 
 <section bind:this={domNode} class="song-selector" class:hidden={$activeAlbum == null}>
-    {#if $songList && $activeAlbum != null}
-        <img class="large-album-cover" src={convertFileSrc($activeAlbum.cover_path)} alt="">
-        <section class="album-info">
-            <header class="mb-05">
-                <h2>{$activeAlbum.title}</h2>
-                <p class="subtitle">{$activeAlbum.artist}</p>
-            </header>
-            <ol class="song-list">
-                {#each $songList as song}
-                    <li class="song">
-                        <!-- so long!!!! -->
-                        <button title={song.title} 
-                            class:active={$currentSong.title == song.title && $currentSong.artist == song.artist}
-                            on:click={() => playSongAndQueue(song)}
-                            on:contextmenu={(e) => showSongMenu(e, song)}>
-                            <span class="track-number">{song.track_number}</span>
-                            <p class="song-title no-wrap">{song.title}</p>
-                            <span class="duration">{sec2time(song.duration)}</span>
-                        </button>
-                    </li>
-                {/each}
-            </ol>
-        </section>
-    {/if}
+    <section class="song-list-wrapper">
+        {#if $songList && $activeAlbum != null}
+            <img class="large-album-cover" src={convertFileSrc($activeAlbum.cover_path)} alt="">
+            <section class="album-info">
+                <header class="mb-05">
+                    <h2>{$activeAlbum.title}</h2>
+                    <p class="subtitle">{$activeAlbum.artist}</p>
+                </header>
+                <ol class="song-list">
+                    {#each $songList as song}
+                        <li class="song">
+                            <!-- so long!!!! -->
+                            <button title={song.title} 
+                                class:active={$currentSong.title == song.title && $currentSong.artist == song.artist}
+                                on:click={() => playSongAndQueue(song)}
+                                on:contextmenu={(e) => showSongMenu(e, song)}>
+                                <span class="track-number">{song.track_number}</span>
+                                <p class="song-title no-wrap">{song.title}</p>
+                                <span class="duration">{sec2time(song.duration)}</span>
+                            </button>
+                        </li>
+                    {/each}
+                </ol>
+            </section>
+        {/if}
+    </section>
     <ContextMenu bind:this={songContextMenu}>
         <Item on:click={playSelectedSongNext}>Play Next</Item>
         <Item on:click={addSelectedToQueue}>Add to Queue</Item>
@@ -94,14 +96,21 @@
 <style>
     .song-selector {
         position: relative;
+        padding: 0 0.5rem;
+        margin-top: 1rem;
+    }
+    
+    .song-list-wrapper {
         display: grid;
         grid-template-columns: 16rem 1fr;
         padding: 1rem;
-        margin-top: 1rem;
         gap: 1rem;
-        background: linear-gradient(white, var(--clr-gray-3));
-        border-top: 1px solid var(--clr-gray-5);
-        border-bottom: 1px solid var(--clr-gray-5);
+        background: var(--clr-gray-3);
+        border-radius: 0.5rem;
+    }
+
+    .large-album-cover {
+        border-radius: 0.25rem;
     }
 
     .song-selector .song-list {
@@ -110,10 +119,10 @@
         column-gap: 3rem;
     }
     
-    .song-selector .song button {
+    .song-list .song button {
         display: grid;
         grid-template-columns: 3ch 1fr auto;
-        padding: 0.25rem 0.5rem;
+        padding: 0.35rem 0.5rem;
         border-radius: 0.25rem;
         gap: 1.5rem;
         transition: all 200ms;
@@ -121,12 +130,11 @@
     }
     
     .song-selector .song button:hover {
-        opacity: 0.75;
+        background: var(--clr-gray-5);
     }
 
-    :global(.song-selector .song button.active) {
-        outline: 1px solid var(--clr-gray-5);
-        background: linear-gradient(white, var(--clr-gray-3));
+    .song-selector .song :global(button.active) {
+        background: var(--clr-gray-0);
     }
     
     .song-selector .song * {
@@ -134,10 +142,6 @@
         text-align: left;
     }
     
-    .song .song-title {
-        font-weight: normal;
-    }
-
     .song .duration {
         float: right;
     }
